166f8c1f311505eb8689aa2d54a5abcf29e31399
santosh52904 <138705693+santosh52904@users.noreply.github.com>
2023-07-06 16:31:25 +0530

    Update order.php

--- order.php
+++ order.php
@@ -1,11 +1,11 @@
 <?php include('partials-front/menu.php'); ?>
 <?php
 //check whether the food id is or not
-if (isset($_GET['food_id'])) {
+if (isset($_GET['film_id'])) {
     //get the details of the food
-    $food_id = $_GET['food_id'];
+    $food_id = $_GET['film_id'];
     //get the details of selected food
-    $sql = "SELECT * FROM tbl_food WHERE  id=$food_id";
+    $sql = "SELECT * FROM tbl_film WHERE  id=$food_id";
     $res = mysqli_query($conn, $sql);
     $count = mysqli_num_rows($res);
     if ($count == 1) {
@@ -18,7 +18,7 @@ if (isset($_GET['food_id'])) {
         $image_name = $row['image_name'];
 
     } else {
-        //food not available
+        //film not available
         header('location:' . SITEURL);
 
 
@@ -30,7 +30,7 @@ if (isset($_GET['food_id'])) {
 }
 
 ?>
-<!-- fOOD sEARCH Section Starts Here -->
+<!-- film sEARCH Section Starts Here -->
 <section class="food-search">
     <div class="container">
 
@@ -40,7 +40,7 @@ if (isset($_GET['food_id'])) {
             <fieldset>
                 <legend>Selected Film</legend>
 
-                <div class="food-menu-img">
+                <div class="film-menu-img">
                     <?php
                     //check whether the image is avilable or not
                     if ($image_name == "") {
@@ -49,7 +49,7 @@ if (isset($_GET['food_id'])) {
 
                     } else {
                         ?>
-                        <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza"
+                        <img src="<?php echo SITEURL; ?>images/film/<?php echo $image_name; ?>" alt="a"
                             class="img-responsive img-curve">
                         <?php
 
@@ -82,24 +82,7 @@ if (isset($_GET['food_id'])) {
                 </div>
 
             </fieldset>
-            <!-- 
-            <fieldset>
-                <legend>Delivery Details</legend>
-                <div class="order-label">Full Name</div>
-                <input type="text" name="full-name" placeholder="E.g. Vijay Thapa" class="input-responsive" required>
-
-                <div class="order-label">Phone Number</div>
-                <input type="tel" name="contact" placeholder="E.g. 9843xxxxxx" class="input-responsive" required>
-
-                <div class="order-label">Email</div>
-                <input type="email" name="email" placeholder="E.g. hi@vijaythapa.com" class="input-responsive" required>
-
-                <div class="order-label">Address</div>
-                <textarea name="address" rows="10" placeholder="E.g. Street, City, Country" class="input-responsive"
-                    required></textarea>
-
-                <input type="submit" name="submit" value="Confirm Order" class="btn btn-primary">
-            </fieldset> -->
+          
 
         </form>
 
@@ -107,31 +90,19 @@ if (isset($_GET['food_id'])) {
         //check whethter the submit button is clicked or not
         if (isset($_POST['submit'])) {
             //get allthe details from the form
-            $food = $_POST['food'];
+            $food = $_POST['film'];
             $price = $_POST['price'];
-            // $qty = $_POST['qty'];
-            // $total = $price * $qty; //total =pricex qty
+          
             $order_date = date("Y-m-d h:i:sa");
             $status = "Ordered"; //ordered,on delivery,cancelled
-            // $customer_name = $_POST['full-name'];
-            // $customer_contact = $_POST['contact'];
-            // $customer_email = $_POST['email'];
-            // $customer_address = $_POST['address'];
-            //save the order in the database
+           
             $sql2 = "INSERT INTO tbl_order SET
-            food='$food',
+            film='$film',
             price='$price',
             order_date='$order_date',
             status='$status'
             ";
-            // total='$total',
-            //  customer_name='$customer_name',
-            //  customer_contact='$customer_contact',
-            //  customer_email='$customer_email',
-            //  customer_address='$customer_address'
-            //   -- qty='$qty',
-            // echo "$sql2";
-            // die();
+         
             //execute the query
             $res2 = mysqli_query($conn, $sql2);
 
